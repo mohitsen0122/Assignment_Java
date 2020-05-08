7020645bd6f4f6834d33450ec1c46b5e5df9cd61
Mohit Sen <mohitsen0122@gmail.com>
2020-05-08 20:29:06 +0530

    Enumeration Prog

---
+++ java/Animals.java
@@ -0,0 +1,13 @@
+package com.practice.java;
+
+enum Animals
+{
+	Tiger(1, 2), Camel(22, 22), Lion(33, 20);
+	private int value;
+	private int a;
+	Animals(int value, int a)
+	{
+		this.value = value;
+		this.a = a;
+	}
+}
